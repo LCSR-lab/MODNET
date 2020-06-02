47be4a9dbb3f2d0c85a2af12c969f0d0fb31f42f
Ariel <ariel@craftech.io>
2020-06-02 17:34:38 -0300

    Add shell script

--- modnet/modnet.py
+++ modnet/modnet.py
@@ -1,8 +1,44 @@
 #!/usr/bin/python3
 
 import click
-from Business import *
-import sys
-import argparse
+import modnet
 
-args = sys.argv
+@click.group()
+@click.option(
+    '--verbose', '-v', 
+    is_flag=True, 
+    help="Will print verbose messages."
+)
+
+@click.option(
+    '--netlist', '-n', 
+    required=True, 
+    help='Path where to find netlist'
+)
+
+@click.option(
+    '--top-module', '-t', 
+    required=True,
+    help='Top module inside netlist'
+)
+
+@click.option(
+    '--outdir', '-o', 
+    default='./output', 
+    help='Path whete to save resulting netlist with injections'
+)
+
+@click.option(
+    '--mode', '-t', 
+    default='synplify',  
+    type=click.Choice(['synplify', 'vivado', 'yosis']),
+    help='Mode defines the netlist origin synthesizer'
+)
+
+@click.pass_context
+def main(verbose,netlist,top_module,mode,outdir):
+    if verbose:
+        click.echo("We are in the verbose mode.")
+    ctx.obj = Analysis(netlist,outdir)
+if __name__ == "__main__":
+    main()
