42ce21a1a9506efe1e4d2b26eba3185379f53011
Ariel <ariel@craftech.io>
2020-06-03 12:17:34 -0300

    Add test for Logic class

--- modnet/tests/00_logic.py
+++ modnet/tests/00_logic.py
@@ -1,10 +1,10 @@
 import pytest
-from .modnet import logic
+from ..logic import Logic
 
-def test_is_combinational(supply_logic):
-    logic_unit = "LUT4"
-    assert logic_unit in supply_logic[0]
+def test_is_combinational():
+    logic_unit = "  LUT4_L"
+    assert Logic.is_combinational(logic_unit)
 
-def test_is_sequential(supply_logic):
-    logic_unit = "FDD"
-    assert logic_unit in supply_logic[1]
+def test_is_sequential():
+    logic_unit = "  FDD"
+    assert Logic.is_sequential(logic_unit)
