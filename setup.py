6f120c18278dc316a2ef1887a11a987a95afd290
Ariel Viñas <ariel@craftech.io>
2020-06-21 20:53:23 -0300

    release/2.0.0-alpha5 (#41)
    * Feature/reduce code (#28)
    
    * reduce and create new modules
    
    * add isort and add missing requirements
    
    * replace for for join
    
    * move logic of modules to other file
    
    * fix isort errors
    
    * remove requiriments
    
    * fix bugs in file output
    
    * fix setup
    
    * make setup work
    
    * mv files to root
    
    * Fix circleci paths (#29)
    
    * Fix circleci paths
    
    * Fix cache
    
    * Fix coverage percentage
    
    * Fix cache in circleci
    
    * Fix circleci paths
    
    * Fix cache
    
    * Fix coverage percentage
    
    * Fix cache key
    
    * Rename jobs in circleci
    
    * Fix circleci paths
    
    * Fix cache
    
    * Fix coverage percentage
    
    * Fix cache key
    
    * Rename jobs
    
    Co-authored-by: Fernan Nestier <fernan.nestier@gmail.com>
    
    * Increased version to v2.0.0-beta3
    
    * Add required packages min versions
    
    * Fix: force use of mayor version 7 of Click
    
    Co-authored-by: nestier <fernan.nestier@gmail.com>
    
    * feature/documentation (#35)
    
    * Add main features and install steps
    
    * Add Readme, badges  and LUT transformation diagram (#38)
    
    Added:
    
    * Changed README to reStructuredText
    * Added basic usage documentation and examples
    * Added LUT transformation diagram
    * Added badges (python version andMODNET last stable version)
    * Linked README.rst to pypi project description implemented in setup.py with `long_description` configuration
    
    Co-authored-by: Fernan Nestier <fernan.nestier@gmail.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import sys
-from pip._internal.req import parse_requirements
+
 from setuptools import find_packages, setup
 
 CURRENT_PYTHON = sys.version_info[:2]
@@ -16,16 +16,23 @@ install it on Python {}.{}.
 """.format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
     sys.exit(1)
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="mod-net",
-    version="v2.0.0-beta2",
+    version="v2.0.0-beta5",
     description="Make SET and SEU fault injections in hierarchical verilog netlists",
-    packages=find_packages(exclude=("tests",)),   
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    packages=find_packages(exclude=("tests",)),
     install_requires=[
-        'Click',
+        'Click==7.*',
     ],
-    entry_points='''
-        [console_scripts]
-        modnet=cli:main
-    ''',
+    entry_points={
+        'console_scripts': ['modnet=modnet.cli:main'],
+    }
 )
